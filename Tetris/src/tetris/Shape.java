271ce47069c835156336f59ca2460ea0b2dea81f
Tatu Forss <tforss@live.fi>
2012-08-26 13:07:31 +0300

    Palikan kääntäminen
    Shape-luokkaan metodit rotatePieceLeft() ja rotatePieceRight(), jotka
    kääntävät palikkaa vasemmalle tai oikealle.

--- Tetris/src/tetris/Shape.java
+++ Tetris/src/tetris/Shape.java
@@ -40,4 +40,48 @@ public class Shape {
         TetrisPiece[] values = TetrisPiece.values();
         setShape(values[x]);
     }
+    
+    public int x(int index) { 
+        return coords[index][0];
+    }
+    public int y(int index) {
+        return coords[index][1];
+    } 
+    
+    private int setX(int index, int x) {
+        return coords[index][0];
+    }
+    
+    private int setY(int index, int y) {
+        return coords[index][1];
+    }
+    
+    public Shape rotatePieceLeft() {
+        if (pieceShape == TetrisPiece.SquareShape) {
+            return this;
+        }
+        
+        Shape result = new Shape();
+        result.pieceShape = pieceShape;
+        
+        for (int i = 0; i < 4; i++) {
+            setX(i, y(i));
+            setY(i, -x(i));
+        }
+        return result;
+    }
+    public Shape rotatePieceRight() {
+        if (pieceShape == TetrisPiece.SquareShape) {
+            return this;
+        }
+        
+        Shape result = new Shape();
+        result.pieceShape = pieceShape;
+        
+        for (int i = 0; i < 4; i++) {
+            setX(i, y(i));
+            setY(-i, x(i));
+        }
+        return result;
+    }
 }
