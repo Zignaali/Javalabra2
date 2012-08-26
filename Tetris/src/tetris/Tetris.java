c79fd34254bfcf16eb4e8fc5f0159383631e64bb
Tatu Forss <tforss@live.fi>
2012-08-26 12:54:02 +0300

    Valikko
    Tetris-luokkaan tehty createMenu() -metodi, joka luo valikon.
    ExitGame() -metodi, joka sulkee ikkunan.
    Tyhjä newGame() -metodi.

--- Tetris/src/tetris/Tetris.java
+++ Tetris/src/tetris/Tetris.java
@@ -15,4 +15,29 @@ public class Tetris implements Runnable {
         window.pack();
         window.setVisible(true);
      }
+     
+     public void createMenu(JFrame window) {
+        JMenuBar menuBar = new JMenuBar();
+        
+        JMenu menu = new JMenu("Game");
+        menuBar.add(menu);
+        
+        JMenuItem menuNewGame = new JMenuItem("New Game");
+        menuNewGame.addActionListener(new menuListener(this));
+        menu.add(menuNewGame);
+  
+        JMenuItem menuExitGame = new JMenuItem("Exit");
+        menuExitGame.addActionListener(new menuListener(this));
+        menu.add(menuExitGame);
+        
+        window.setJMenuBar(menuBar);
+     }
+     
+     public void newGame() {
+         
+     }
+     
+     public void exitGame() {
+         window.dispose();
+     }
 }
