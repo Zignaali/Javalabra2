45a99caedf9912eb561b856b6458caaa49d878ff
Tatu Forss <tforss@live.fi>
2012-08-26 13:00:11 +0300

    Shape-luokka
    Shape-luokan luonti, sen konstruktori ja setShape() -metodi, joka
    sisältää koordinaatiston ja kaikkien erilaisten tetrispalikoiden
    koordinaatit.

---
+++ Tetris/src/tetris/Shape.java
@@ -0,0 +1,35 @@
+
+package tetris;
+
+
+public class Shape {
+    private int[][] coords;
+    private int[][][] coordsTable;
+    enum TetrisPiece { NoShape, ZShape, SShape, LineShape, TShape, SquareShape,
+                        LShape, MirroredLShape };
+    private TetrisPiece pieceShape;
+    
+    public Shape() {
+        coords = new int[4][2];
+        setShape(TetrisPiece.NoShape);
+    }
+    
+    public void setShape(TetrisPiece shape) {
+        coordsTable = new int[][][] {
+            { { 0, 0 },   { 0, 0 },   { 0, 0 },   { 0, 0 } }, // NoShape
+            { { 0, -1 },  { 0, 0 },   { -1, 0 },  { -1, 1} }, // SShape
+            { { 0, -1 },  { 0, 0 },   { 1, 0 },   { 1, 1 } }, // ZShape
+            { { 0, -1 },  { 0, 0 },   { 0, 1 },   { 0, 2 } }, // LineShape
+            { { -1, 0 },  { 0, 0 },   { 1, 0 },   { 0, 1 } }, // TShape
+            { { 0, 0 },   { 1, 0 },   { 0, 1 },   { 1, 1 } }, // SquareShape
+            { { -1, -1 }, { 0, -1 },  { 0, 0 },   { 0, 1 } }, // MirroredLShape
+            { { 1, -1 },  { 0, -1 },  { 0, 0 },   { 0, 1 } } }; // LShape 
+        
+        for (int i = 0; i < 4 ; i++) {
+            for (int j = 0; j < 2; ++j) {
+                coords[i][j] = coordsTable[shape.ordinal()][i][j];
+            }
+        }
+        pieceShape = shape;
+    }
+}
